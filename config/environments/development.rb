edf6b058003cab3b95de23ebc606e8cd8c5bb94b
daniiielak <danielaelisabeth.kepper@gmail.com>
2018-03-22 00:05:26 +0000

    added products database

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,9 @@
 Rails.application.configure do
+  
+  class Application < Rails::Application
+  config.web_console.whitelisted_ips = '37.228.246.54'
+  end
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -51,4 +56,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
 end
