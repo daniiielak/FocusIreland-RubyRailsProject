2625281bde17943ab375004daeca62fb62873ad9
Cathy-Orla <catherine.lane@ucdconnect.ie>
2018-04-06 15:02:29 +0000

    Change password function added

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,11 @@ Rails.application.routes.draw do
   resource :thankyous, only: [:show]
   #get '/thankyous'=> 'thankyous#index'
   
+  devise_for "users", :skip => [:registrations]
+  as :user do
+   get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_regsitration'
+   put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   root 'site#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
